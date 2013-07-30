e2a5e655e4cc85e6377e5c2143543c5b80c67028
Chief244 <Chief@Chief-PC>
2013-07-30 13:48:38 -0700

    Animated Some pixels

--- Maze/src/Display.java
+++ Maze/src/Display.java
@@ -23,7 +23,7 @@ public class Display extends Canvas implements Runnable {
 
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
-	public static final String TITLE = "Project Y Pre-Alpha 0.01";
+	public static final String TITLE = "Maze Pre-Alpha 0.01";
 
 	private Thread thread;
 	private Render render;
